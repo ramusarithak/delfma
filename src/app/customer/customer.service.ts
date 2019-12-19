97084a6d32d67c611ca04f547013bfc6e8e09732
Ramu Saritha K <ramusarithak@gmail.com>
2019-12-19 10:35:55 +0530

    deleteuser

---
+++ src/app/customer/customer.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(private http: HttpClient) { }
+
+  customerAdd(data) {
+    return this.http.post(environment.apiUrl + 'customer', data)
+  }
+
+  getBySpId(id) {
+    return this.http.get(environment.apiUrl + 'getcustomerbyserviceprovider/' + id)
+  }
+
+  getCusById(id) {
+    return this.http.get(environment.apiUrl + 'getcustomerbyId/' + id)
+  }
+
+    updateCustomer(id, data) {
+    return this.http.put(environment.apiUrl + 'customer/' + id, data)
+  }
+  deleteCustomer(id){
+    // console.log("service Inside customer id --->",id)
+
+    return this.http.get(environment.apiUrl + 'deletecustomer/' + id)
+  }
+}
